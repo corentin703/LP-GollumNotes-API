b1d75862df5455d1ea7e9cda35a7280868be4f55
Corentin VÉROT <corentin.verot@outlook.com>
2022-06-02 11:40:16 +0200

    Heroku DB

--- CoverotNimorin.GollumChat.Server/Program.cs
+++ CoverotNimorin.GollumChat.Server/Program.cs
@@ -95,5 +95,5 @@ string GetHerokuConnectionString()
     string db = databaseUri.LocalPath.TrimStart('/');
     string[] userInfo = databaseUri.UserInfo.Split(':', StringSplitOptions.RemoveEmptyEntries);
 
-    return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};Database={db};Pooling=true;SSL Mode=Require;Trust Server Certificate=True;";
+    return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};Port={databaseUri.Port};Database={db};sslmode=Require;TrustServerCertificate=True;";
 }
